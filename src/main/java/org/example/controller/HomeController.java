7788308f62f97ab026e3ef39f4ea093936e4da8f
ginsenglee <thecynicdog0328@gmail.com>
2023-08-20 14:59:43 +0900

    logout

--- src/main/java/org/example/controller/HomeController.java
+++ src/main/java/org/example/controller/HomeController.java
@@ -5,7 +5,9 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.SessionHandler;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.sstore.LocalSessionStore;
 import org.example.entity.users.User;
 import org.example.projection.UserProjection;
 import org.example.service.UserService;
@@ -28,7 +30,7 @@ public class HomeController implements Controller {
     public void registerRoutes(Vertx vertx, Router router) {
 
         router.route().handler(StaticHandler.create());
-
+        router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx)));
         {   // GET handlers
             router.get("/home").handler(routingContext -> {
                 routingContext.response().putHeader("Content-Type", "text/html").sendFile(PUBLIC + "home.html");
